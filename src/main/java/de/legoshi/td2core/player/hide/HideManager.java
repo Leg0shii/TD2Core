37d5f59438e70dc0e6d82f0230c3195acf6728c5
Leg0shii <dragonsaremyfavourite@gmail.com>
2024-10-07 13:46:47 +0200

    remove paper relict

--- src/main/java/de/legoshi/td2core/player/hide/HideManager.java
+++ src/main/java/de/legoshi/td2core/player/hide/HideManager.java
@@ -19,7 +19,7 @@ public class HideManager implements Listener {
     public void hidePlayer(Player player, String name) {
         Player playerToHide = Bukkit.getPlayer(name);
         if (playerToHide != null) {
-            player.hidePlayer(TD2Core.getInstance(), playerToHide);
+            player.hidePlayer(playerToHide);
             hidePlayers.computeIfAbsent(player, k -> new ArrayList<>()).add(playerToHide);
             player.sendMessage(Message.SUCCESS_HIDE_PLAYER.getSuccessMessage(name));
         } else {
@@ -30,7 +30,7 @@ public class HideManager implements Listener {
     public void unHidePlayer(Player player, String name) {
         Player playerToUnHide = Bukkit.getPlayer(name);
         if (playerToUnHide != null && hidePlayers.containsKey(player)) {
-            player.showPlayer(TD2Core.getInstance(), playerToUnHide);
+            player.showPlayer(playerToUnHide);
             hidePlayers.get(player).remove(playerToUnHide);
             player.sendMessage(Message.SUCCESS_SHOW_PLAYER.getSuccessMessage(name));
         } else {
@@ -39,13 +39,13 @@ public class HideManager implements Listener {
     }
     
     public void hideAllPlayers(Player player) {
-        Bukkit.getOnlinePlayers().forEach(all -> player.hidePlayer(TD2Core.getInstance(), all));
+        Bukkit.getOnlinePlayers().forEach(all -> player.hidePlayer(all));
         hideAllPlayers.add(player);
         player.sendMessage(Message.SUCCESS_HIDE_ALL.getSuccessMessage());
     }
     
     public void unHideAllPlayers(Player player) {
-        Bukkit.getOnlinePlayers().forEach(all -> player.showPlayer(TD2Core.getInstance(), all));
+        Bukkit.getOnlinePlayers().forEach(all -> player.showPlayer(all));
         hideAllPlayers.remove(player);
         hidePlayers.remove(player);
         player.sendMessage(Message.SUCCESS_SHOW_ALL.getSuccessMessage());
@@ -54,11 +54,11 @@ public class HideManager implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player joinedPlayer = event.getPlayer();
-        hideAllPlayers.forEach(p -> p.hidePlayer(TD2Core.getInstance(), joinedPlayer));
+        hideAllPlayers.forEach(p -> p.hidePlayer(joinedPlayer));
         hidePlayers.forEach((k, playerListToHide) ->
             playerListToHide.stream()
                 .filter(p -> p.getName().equals(joinedPlayer.getName()))
-                .forEach(p -> k.hidePlayer(TD2Core.getInstance(), joinedPlayer))
+                .forEach(p -> k.hidePlayer(joinedPlayer))
         );
     }
     
